e573a135404e7c4339224ad1116aefd28c5fef53
soundoftherain <m47612815@gmail.com>
2025-09-30 16:59:32 +0200

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="py-10 bg-gray-900 text-white text-center">
+      <p>&copy; 2025 Noah's Art</p>
+      <p className="mt-2 italic">The storm is not the end. It is the beginning.</p>
+    </footer>
+  )
+}
