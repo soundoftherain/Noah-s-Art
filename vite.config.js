f1c4401404f7a146f61a7807f7ca29c8c9e9c622
soundoftherain <m47612815@gmail.com>
2025-10-01 13:26:18 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
   base: '/noah-s-art/', // 👈 matches your repo name
 })
