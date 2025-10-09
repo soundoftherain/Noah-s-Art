ff3c6d73000a3d764f667680e6db78868ed4035d
soundoftherain <m47612815@gmail.com>
2025-10-09 15:15:45 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/noah-s-art/', // 👈 matches your repo name
-})
