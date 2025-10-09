b6ddd3b16964f5e2299292ca72c54f3cbf857fc6
soundoftherain <m47612815@gmail.com>
2025-10-09 18:03:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Noah-s-Art/", // <--- Add this line
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
 });
