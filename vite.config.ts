358d3ca9e388c6d118515ca5e0c3ab1c16253e51
soundoftherain <m47612815@gmail.com>
2025-10-10 10:37:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 👈 this is critical for custom domains
+  build: {
+    outDir: "dist", // ensures the build goes into dist
+  },
   server: {
     port: 5173,
   },
